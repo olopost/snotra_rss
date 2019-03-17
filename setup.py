439ed6e96059a7e0fd415ec881f99fdcd24c916c
samuel <samuel@meyn.fr>
2019-03-17 15:19:51 +0100

    add scheduler - test1

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+with open("requirements.txt", "r") as rp:
+    req = rp.read()
 
 setuptools.setup(
     name="snotra_rss",
@@ -12,6 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/olopost/snotra_rss",
+    install_requires=req,
     packages=setuptools.find_packages(),
     package_dir={'snotra_rss': 'snotra_rss'},
     package_data={
